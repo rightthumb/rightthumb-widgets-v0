33faddf8e686bb5a964ac4c36b74eb2dff6c94db
Scott Reph <scott.reph@gmail.com>
2022-03-30 16:13:27 -0400

    {6786e184-8229-4cbb-epoc-164867120602}

--- widgets/batch/git.cp.bat
+++ widgets/batch/git.cp.bat
@@ -1,5 +1,8 @@
 @echo off
+call m back --c
+call b w > nul
 rem call p lock-files -w
 call uuid -e | p -copy
 git commit
 git push
+call b back > nul
---
+++ widgets/batch/git.files.bat
@@ -0,0 +1,16 @@
+@echo off
+call m back --c
+call b w > nul
+if [%1] == [] (
+	call:one
+) else (
+	call:two %*
+)
+call b back > nul
+goto:eof
+:one
+call p files -w --c -ago 10h | p line --c -make "git add {}" | p execute
+goto:eof
+:two
+call p files -w --c -ago %* | p line --c -make "git add {}" | p execute
+goto:eof
