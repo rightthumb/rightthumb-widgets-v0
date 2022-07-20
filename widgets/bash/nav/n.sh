c7aa047dcbdafa4a5c9b63930777198f1d982e6e
eyeformeta <scottreph@gmail.com>
2022-07-20 01:01:29 -0400

    {88e05643-3641-4c02-epoc-165829328838}

--- widgets/bash/nav/n.sh
+++ widgets/bash/nav/n.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # ## {R2D2919B742E} ##
 # ###########################################################################
 # What if magic existed?
@@ -9,7 +8,6 @@
 #    - Scott Taylor Reph, RightThumb.com
 # ###########################################################################
 # ## {C3P0D40fAe8B} ##
-
 # SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # SCRIPT_DIR="${SCRIPT_DIR/bash\/nav/bash}"
 # source  "$SCRIPT_DIR/load-vars.sh"
@@ -19,23 +17,21 @@ subject_path=$subject
 # subject_path=./subject
 p="bash $widgets/widgets/bash/nav/p.sh"
 $p fileBackup -i $subject_path
-if [ "'$code_editor'_pre" = "" -a "'$code_editor'_suff" = "" ]; then
+if [ "$code_editor_pre" = "" -a "$code_editor_suff" = "" ]; then
     echo 1
-    '$code_editor' $subject_path
+    $code_editor $subject_path
 else
-    if [ "'$code_editor'_pre" != "" -a "'$code_editor'_suff" != "" ]; then
+    if [ "$code_editor_pre" != "" -a "$code_editor_suff" != "" ]; then
         echo 2
-        '$code_editor'_pre '$code_editor' $subject_path '$code_editor'_suff >/dev/null 2>&1
+        $code_editor_pre $code_editor $subject_path $code_editor_suff >/dev/null 2>&1
     else
-        if [ "'$code_editor'_pre" != "" ]; then
+        if [ "$code_editor_pre" != "" ]; then
             echo 3
-            '$code_editor'_pre '$code_editor' $subject_path
+            $code_editor_pre $code_editor $subject_path
         else
             echo 4
-            '$code_editor' $subject_path '$code_editor'_suff >/dev/null 2>&1
+            $code_editor $subject_path $code_editor_suff >/dev/null 2>&1
         fi
     fi
 fi
-# '$code_editor'_pre '$code_editor' $subject_path '$code_editor'_suff>/dev/null 2>&1
-
-
+# $code_editor_pre $code_editor $subject_path $code_editor_suff>/dev/null 2>&1
