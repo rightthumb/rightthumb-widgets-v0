fc5893cb17caa7865ddce2da4dd114c76d8a55ca
eyeformeta <scottreph@gmail.com>
2023-04-07 01:27:33 -0400

    {d79c4cd2-2715-4bc2-epoc-168084525276}

--- widgets/batch/.d.bat
+++ widgets/batch/.d.bat
@@ -7,7 +7,7 @@ SET /p pretty_back=<%tmpf%-back
 echo purple %pretty_back%  >> %tmpf%-print_color-day
 echo.                                >> %tmpf%-print_color-day
 echo darkcyan                     >> %tmpf%-print_color-day
-echo cyan C:\Users\Scott\.rt\profile\daily\2023\14\04-06\                       >> %tmpf%-print_color-day
+echo cyan C:\Users\Scott\.rt\profile\daily\2023\14\04-07\                       >> %tmpf%-print_color-day
 echo Background.green linePrint      >> %tmpf%-print_color-day
 call p print_color -f %tmpf%-print_color-day -tab 2
-cd C:\Users\Scott\.rt\profile\daily\2023\14\04-06\
+cd C:\Users\Scott\.rt\profile\daily\2023\14\04-07\
