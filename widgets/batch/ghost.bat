78434dc56a93ff858a4b443b49b70950bb3f690f
rightthumb <scott.reph@gmail.com>
2023-10-03 01:29:36 -0400

    {a114b9c4-4c48-4f18-epoc-169631097663}

--- widgets/batch/ghost.bat
+++ widgets/batch/ghost.bat
@@ -1,10 +1,10 @@
 @echo off
 if [%2] == [-y] (
-	call p MoveDelete -src %1 -ghost all -y
+	call p MoveDelete -src %1 -ghost -y
 	goto:eof
 )
 if [%2] == [y] (
-	call p MoveDelete -src %1 -ghost all -y
+	call p MoveDelete -src %1 -ghost -y
 	goto:eof
 )
-call p MoveDelete -src %1 -ghost all
+call p MoveDelete -src %1 -ghost
