18a9db616a42fccdb905231aa33da8b38ef9db99
softwaredevsolutions <scott@softwaredevelopment.solutions>
2024-07-02 16:22:02 -0600

    {05f4fd71-a831-4972-epoc-171995892238}

--- install/installer.py
+++ install/installer.py
@@ -3227,6 +3227,7 @@ _title() {
 
 alias ovpn="source $widgets/widgets/bash/openvpn_manager.sh"
 alias ovpn.="source $widgets/widgets/bash/openvpn_manager.sh"
+alias ...ssh="ssh-keygen -t rsa -b 4096"
 
 if [ -f "${stmp}/pin_ask" ]; then
 	source $widgets/widgets/bash/pin.sh
