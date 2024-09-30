e6cce98b3eddb0c0e6a29a3f93ab599255670ce8
softwaredevsolutions <scott@softwaredevelopment.solutions>
2024-09-29 22:57:57 -0400

    {4db66b1f-4285-4f79-epoc-172766507781}

--- widgets/bash/on.fi.sh
+++ widgets/bash/on.fi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$2" ]; then
-	$p: tag -trigger expires 1w -f "$@"
-	$p: on -session "$Session_id" -folder .
+	$p tag -trigger expires 1w -f "$@"
+	$p on -session "$Session_id" -folder .
 else
-	$p: tag -trigger expires 1w -delete -f "$@"
-	$p: on -session "$Session_id" -folder . -delete
+	$p tag -trigger expires 1w -delete -f "$@"
+	$p on -session "$Session_id" -folder . -delete
 fi
--- widgets/bash/on.fo.sh
+++ widgets/bash/on.fo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	$p: on -session "$Session_ID" -folder "$(pwd)"
-	$p: tag -trigger expires 1w -fo .
+	$p on -session "$Session_ID" -folder "$(pwd)"
+	$p tag -trigger expires 1w -fo .
 else
-	$p: on -session "$Session_ID" -delete
-	$p: tag -trigger expires 1w -fo . -delete
+	$p on -session "$Session_ID" -delete
+	$p tag -trigger expires 1w -fo . -delete
 fi
