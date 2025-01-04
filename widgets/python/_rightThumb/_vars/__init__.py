e3c70c7cf435d7a86008a112615d9288bfd690a4
softwaredevsolutions <scott@softwaredevelopment.solutions>
2025-01-04 18:59:16 -0500

    {f437b4e2-4331-4a7d-epoc-173603515670}

--- widgets/python/_rightThumb/_vars/__init__.py
+++ widgets/python/_rightThumb/_vars/__init__.py
@@ -1478,10 +1478,10 @@ def get_administrator_sid():
 		temp_file_name = temp_file.name
 
 	try:
-		try:
-			os.system(f'wmic useraccount where "name=\'administrator\' and domain=\'%computername%\'" get name,sid | find "admin" > {temp_file_name}')
-		except:
-			os.system(f'powershell -Command "Get-WmiObject Win32_UserAccount | Where-Object {{ $_.Name -eq \'administrator\' }} | Select-Object Name,SID | Out-File -FilePath {temp_file_name}"')
+		# os.system(f'wmic useraccount where "name=\'administrator\' and domain=\'%computername%\'" get name,sid | find "admin" > {temp_file_name}')
+		os.system(f'powershell -Command "Get-WmiObject Win32_UserAccount | Where-Object {{ $_.Name -eq \'administrator\' }} | Select-Object Name,SID | Out-File -FilePath {temp_file_name}"')
+		# try:
+		# except:
 		
 		with open(temp_file_name, 'r') as temp_file:
 			output = temp_file.read()
